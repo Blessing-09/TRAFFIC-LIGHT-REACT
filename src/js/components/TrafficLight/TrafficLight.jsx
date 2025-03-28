3a44156a6224ccaacb8dec804f976baa0db21e09
Blessing-09 <blessingogbogu@gmail.com>
2025-03-28 04:48:00 +0100

    Stop button pending

--- src/js/components/TrafficLight/TrafficLight.jsx
+++ src/js/components/TrafficLight/TrafficLight.jsx
@@ -2,38 +2,8 @@ import "./TrafficLight.css";
 import React, { useState, useEffect } from "react";
 
 const TrafficLight = () => {
-  // const redTraffic = {
-  //   backgroundColor: "red",
-  //   border: "3px solid #ac0a0a",
-  //   width: "100px",
-  //   height: "100px",
-  //   borderRadius: "50%",
-  //   display: "inline-block",
-  //   margin: "3px",
-  //   cursor: "pointer",
-  // };
-  // const yellowTraffic = {
-  //   backgroundColor: "yellow",
-  //   border: "3px solid #dae508",
-  //   width: "100px",
-  //   height: "100px",
-  //   borderRadius: "50%",
-  //   display: "inline-block",
-  //   margin: "3px",
-  //   cursor: "pointer",
-  // };
-  //   const greenTraffic = {
-  //     backgroundColor: "green",
-  //     border: "3px solidrgb(70, 67, 6);",
-  //     width: "100px",
-  //     height: "100px",
-  //     borderRadius: "50%",
-  //     display: "inline-block",
-  //     margin: "3px",
-  //     cursor: "pointer"
-  //   };
-  // Used this function to simplify the current traffic color characteristics
-  const currentTraffic = color => ({
+  // current traffic lights as a function with a given parameter
+  const currentTraffic = (color) => ({
     backgroundColor: color,
     border: "3px solid ${color}",
     width: "100px",
@@ -41,59 +11,99 @@ const TrafficLight = () => {
     borderRadius: "50%",
     display: "inline-block",
     margin: "3px",
-    cursor: "pointer"
+    cursor: "pointer",
   });
   // This function allow us to change the state of the traffic color
-  const activeTraffic = (color) =>({
+  const activeTraffic = (color) => ({
     backgroundColor: color,
     width: "100px",
     height: "100px",
     borderRadius: "50%",
     boxShadow: "-2px -1px 100px 30px rgba(253, 248, 216, 0.75)",
-    margin: "3px"
+    margin: "3px",
   });
 
   const [activeLight, setActiveLight] = useState({
     red: currentTraffic("red"),
     yellow: currentTraffic("yellow"),
-    green: currentTraffic("green") ,
-  }
-  );
-//  This function allows us toggle between each traffic light
-  let  activeColorChange = (color) => {
-    setActiveLight( () => ({
-     red: color === "red" ? activeTraffic("red") : currentTraffic("red"),
-     yellow: color === "yellow" ? activeTraffic("yellow") : currentTraffic("yellow"),
-     green: color === "green" ? activeTraffic("green") : currentTraffic("green"),
+    green: currentTraffic("green"),
+  });
+  const [activeLightColor, setActiveLightColor] = useState("green");
+  const [running, setRunning] = useState(false);
+
+  //  This function allows us toggle between each traffic light when clicked
+  const activeColorChange = (color) => {
+    setActiveLight(() => ({
+      red: color === "red" ? activeTraffic("red") : currentTraffic("red"),
+      yellow:
+        color === "yellow" ? activeTraffic("yellow") : currentTraffic("yellow"),
+      green:
+        color === "green" ? activeTraffic("green") : currentTraffic("green"),
     }));
   };
- 
+
+  const atInterval = () => {
+    if (running) return;
+    setRunning(true);
+
+    let color = activeLightColor;
+
+    const intervalId = setInterval(() => {
+      switch (color) {
+        case "red":
+          color = "yellow";
+          break;
+        case "yellow":
+          color = "green";
+          break;
+        case "green":
+          color = "red";
+          break;
+        default:
+          color = "red";
+      }
+      setActiveLightColor(activeColorChange(color));
+    }, 2000);
+
+    const stop = () => {clearInterval(intervalId);
+    setRunning(false);
+   
+    };
+    
+    return stop;
+    
+  };
+
 
   return (
     <div className="container d-flex justify-content-center align-items-center">
       <div className="traffic-top"></div>
       <div className="traffic-container">
         <div
+        data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to display"
           style={activeLight.red}
           className="traffic-light red"
-          onClick={() => activeColorChange("red")
-        }
+          onClick={() => activeColorChange("red")}
         ></div>
         <div
+        data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to display"
           style={activeLight.yellow}
           className="traffic-light yellow"
-          onClick={() => activeColorChange("yellow")
-        }
+          onClick={() => activeColorChange("yellow")}
         ></div>
         <div
+        data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to display"
           style={activeLight.green}
           className="traffic-light green"
-          onClick={() => activeColorChange("green")
-          }
+          onClick={() => activeColorChange("green")}
         ></div>
-
       </div>
-      <button className="btn btn-primary">CHANGE LIGHTS!</button>
+      <button 
+      data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to toggle"
+      className="btn btn-primary" onClick={() => atInterval()}>
+        CHANGE LIGHTS!
+      </button>
+      <button onClick={() => {stop}} className="btn btn-danger">STOP</button>
     </div>
   );
 };
