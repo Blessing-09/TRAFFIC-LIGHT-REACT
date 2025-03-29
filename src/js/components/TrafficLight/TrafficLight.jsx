a38ea6365764e4b590b0a460eb2b7ae3b4c58227
Blessing-09 <blessingogbogu@gmail.com>
2025-03-29 15:56:55 +0100

    Debugging and updating changes

--- src/js/components/TrafficLight/TrafficLight.jsx
+++ src/js/components/TrafficLight/TrafficLight.jsx
@@ -1,5 +1,5 @@
 import "./TrafficLight.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const TrafficLight = () => {
   // current traffic lights as a function with a given parameter
@@ -21,6 +21,7 @@ const TrafficLight = () => {
     borderRadius: "50%",
     boxShadow: "-2px -1px 100px 30px rgba(253, 248, 216, 0.75)",
     margin: "3px",
+    filter: "drop-shadow(0 0 10px rgba(126, 122, 122, 0.9))",
   });
 
   const [activeLight, setActiveLight] = useState({
@@ -28,9 +29,9 @@ const TrafficLight = () => {
     yellow: currentTraffic("yellow"),
     green: currentTraffic("green"),
   });
-  const [activeLightColor, setActiveLightColor] = useState("green");
+  const [activeLightColor, setActiveLightColor] = useState("");
   const [running, setRunning] = useState(false);
-
+  const intervalId = useRef(null);
   //  This function allows us toggle between each traffic light when clicked
   const activeColorChange = (color) => {
     setActiveLight(() => ({
@@ -42,13 +43,13 @@ const TrafficLight = () => {
     }));
   };
 
-  const atInterval = () => {
+  const runInterval = () => {
     if (running) return;
     setRunning(true);
 
     let color = activeLightColor;
 
-    const intervalId = setInterval(() => {
+    intervalId.current = setInterval(() => {
       switch (color) {
         case "red":
           color = "yellow";
@@ -65,45 +66,65 @@ const TrafficLight = () => {
       setActiveLightColor(activeColorChange(color));
     }, 2000);
 
-    const stop = () => {clearInterval(intervalId);
-    setRunning(false);
-   
-    };
-    
-    return stop;
-    
+    return () => clearInterval(intervalId);
   };
 
+  function stopInterval() {
+    setRunning(false);
+    clearInterval(intervalId.current);
+    // reset to original states
+    setActiveLight({
+      red: currentTraffic("red"),
+      yellow: currentTraffic("yellow"),
+      green: currentTraffic("green"),
+    });
+    setActiveLightColor("");
+  }
 
   return (
     <div className="container d-flex justify-content-center align-items-center">
       <div className="traffic-top"></div>
       <div className="traffic-container">
         <div
-        data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to display"
+          data-bs-toggle="tooltip"
+          data-bs-placement="bottom"
+          title="Click to display"
           style={activeLight.red}
           className="traffic-light red"
           onClick={() => activeColorChange("red")}
         ></div>
         <div
-        data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to display"
+          data-bs-toggle="tooltip"
+          data-bs-placement="bottom"
+          title="Click to display"
           style={activeLight.yellow}
           className="traffic-light yellow"
           onClick={() => activeColorChange("yellow")}
         ></div>
         <div
-        data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to display"
+          data-bs-toggle="tooltip"
+          data-bs-placement="bottom"
+          title="Click to display"
           style={activeLight.green}
           className="traffic-light green"
           onClick={() => activeColorChange("green")}
         ></div>
       </div>
-      <button 
-      data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click to toggle"
-      className="btn btn-primary" onClick={() => atInterval()}>
-        CHANGE LIGHTS!
-      </button>
-      <button onClick={() => {stop}} className="btn btn-danger">STOP</button>
+
+      <div className="button-container d-flex flex-column justify-content-center ms-3 gap-3">
+        <button
+          data-bs-toggle="tooltip"
+          data-bs-placement="bottom"
+          title="Click to toggle"
+          className="btn btn-primary"
+          onClick={() => runInterval()}
+        >
+          TOGGLE
+        </button>
+        <button onClick={() => stopInterval()} className="btn btn-danger">
+          STOP
+        </button>
+      </div>
     </div>
   );
 };
